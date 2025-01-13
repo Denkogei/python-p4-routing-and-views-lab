44e9b3066498aa71df36c14ac9bce45d4c81ddf7
Dennis Kemboi <denkogei11@gmail.com>
2025-01-13 13:32:03 +0300

    flask routing nd views lab

--- server/app.py
+++ server/app.py
@@ -4,5 +4,43 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Index route
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+# Print route
+@app.route('/print/<string:text>')
+def print_string(text):
+    print(text)  
+    return text  
+
+# Count route
+@app.route('/count/<int:number>')
+def count(number):
+    numbers_list = "\n".join(str(i) for i in range(number)) + '\n' 
+    return numbers_list  
+
+# Math route
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == '/' or operation == 'div':  
+        if num2 == 0:
+            return "Error: Division by zero is not allowed."
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return "Invalid operation"
+    
+    return str(result)  
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
